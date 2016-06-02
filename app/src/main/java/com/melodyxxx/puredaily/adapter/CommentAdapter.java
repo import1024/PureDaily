c73f6fdb8b2a867ecd6b1cd4367d43d731e7bf85
bluehan <95hanjie@gmail.com>
2016-06-02 23:38:04 +0800

    merge long&short comment

--- app/src/main/java/com/melodyxxx/puredaily/adapter/CommentAdapter.java
+++ app/src/main/java/com/melodyxxx/puredaily/adapter/CommentAdapter.java
@@ -24,40 +24,97 @@ import java.util.ArrayList;
  */
 public class CommentAdapter extends BaseAdapter<CommentAdapter.MyViewHolder> {
 
+    private static final int TYPE_COMMENT = 0;
+    private static final int TYPE_LONG_COMMENT_HEADER = 1;
+    private static final int TYPE_SHORT_COMMENT_HEADER = 2;
+
+    // 长评在评论集合中的起始位置
+    private int mLongCommentStartPos = -1;
+
+    // 短评在评论集合中的起始位置
+    private int mShortCommentStatPos = -1;
+
     private ArrayList<Comment> mComments;
     private Context mContext;
 
-    public CommentAdapter(Context context, ArrayList<Comment> comments) {
+    private View mCommentHeaderView;
+
+    /**
+     * @param context
+     * @param comments            评论集合
+     * @param longCommentStartPos 长评论在集合中的起始位置，负数代表没有长评论
+     * @param shortCommentStatPos 短评论在集合中的起始位置，负数代表没有短评论
+     */
+    public CommentAdapter(Context context, ArrayList<Comment> comments, int longCommentStartPos, int shortCommentStatPos) {
         this.mContext = context;
         mComments = comments;
+        this.mLongCommentStartPos = longCommentStartPos;
+        this.mShortCommentStatPos = shortCommentStatPos;
+        // 根据是否有长、短评论设置不同的长度
+        if (hasLongComment()) {
+            // 添加一个null作为Comment Header的占位，以便后面取Comment时position的统一
+            this.mComments.add(0, null);
+            // 短评起始位置需要偏移
+            mShortCommentStatPos++;
+        }
+        if (hasShortComment()) {
+            this.mComments.add(mShortCommentStatPos, null);
+        }
     }
 
     @Override
     public void onBindViewHolder(MyViewHolder holder, int position) {
         super.onBindViewHolder(holder, position);
-        Comment comment = mComments.get(position);
-        Glide.with(mContext)
-                .load(comment.getAvatar())
-                .diskCacheStrategy(DiskCacheStrategy.ALL)
-                .crossFade()
-                .into(holder.avatar);
-        holder.author.setText(comment.getAuthor());
-        holder.time.setText(CommonUtils.formatCommentTime(Long.parseLong(comment.getTime() + "000")));
-        holder.content.setText(comment.getContent());
-        holder.likeCount.setText(comment.getLikes());
+        if (position == mLongCommentStartPos) {
+            holder.commentType.setText("长评");
+        } else if (position == mShortCommentStatPos) {
+            holder.commentType.setText("短评");
+        } else {
+            Comment comment = mComments.get(position);
+            Glide.with(mContext)
+                    .load(comment.getAvatar())
+                    .diskCacheStrategy(DiskCacheStrategy.ALL)
+                    .crossFade()
+                    .into(holder.avatar);
+            holder.author.setText(comment.getAuthor());
+            holder.time.setText(CommonUtils.formatCommentTime(Long.parseLong(comment.getTime() + "000")));
+            holder.content.setText(comment.getContent());
+            holder.likeCount.setText(comment.getLikes());
+        }
         // 开启动画
         setAnimation(holder.itemView);
     }
 
-    public void syncData(ArrayList<Comment> comments) {
-        this.mComments = comments;
+    @Override
+    public int getItemViewType(int position) {
+        if (position == mLongCommentStartPos) {
+            return TYPE_LONG_COMMENT_HEADER;
+        }
+        if (position == mShortCommentStatPos) {
+            return TYPE_SHORT_COMMENT_HEADER;
+        }
+        return TYPE_COMMENT;
+
     }
+
+    private boolean hasLongComment() {
+        return mLongCommentStartPos >= 0;
+    }
+
+    private boolean hasShortComment() {
+        return mShortCommentStatPos >= 0;
+    }
+
 
     @Override
     public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         LayoutInflater inflater = LayoutInflater.from(mContext);
-        View view = inflater.inflate(R.layout.item_comment, parent, false);
-        return new MyViewHolder(view);
+        if (viewType == TYPE_LONG_COMMENT_HEADER || viewType == TYPE_SHORT_COMMENT_HEADER) {
+            mCommentHeaderView = inflater.inflate(R.layout.item_comment_header, parent, false);
+            return new MyViewHolder(mCommentHeaderView);
+        } else {
+            return new MyViewHolder(inflater.inflate(R.layout.item_comment, parent, false));
+        }
     }
 
     @Override
@@ -67,6 +124,8 @@ public class CommentAdapter extends BaseAdapter<CommentAdapter.MyViewHolder> {
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
 
+        TextView commentType;
+
         ImageView avatar;
         TextView author;
         TextView time;
@@ -75,6 +134,10 @@ public class CommentAdapter extends BaseAdapter<CommentAdapter.MyViewHolder> {
 
         public MyViewHolder(View itemView) {
             super(itemView);
+            if (itemView == mCommentHeaderView) {
+                commentType = (TextView) itemView.findViewById(R.id.tv_comment_type);
+                return;
+            }
             avatar = (ImageView) itemView.findViewById(R.id.iv_avatar);
             author = (TextView) itemView.findViewById(R.id.tv_author);
             time = (TextView) itemView.findViewById(R.id.tv_time);
--- app/src/main/java/com/melodyxxx/puredaily/ui/activity/CommentActivity.java
+++ app/src/main/java/com/melodyxxx/puredaily/ui/activity/CommentActivity.java
@@ -11,9 +11,7 @@ import android.util.Log;
 import android.view.View;
 
 import com.melodyxxx.puredaily.R;
-import com.melodyxxx.puredaily.adapter.BaseAdapter;
 import com.melodyxxx.puredaily.adapter.CommentAdapter;
-import com.melodyxxx.puredaily.adapter.LatestAdapter;
 import com.melodyxxx.puredaily.entity.Comment;
 import com.melodyxxx.puredaily.entity.Latest;
 import com.melodyxxx.puredaily.task.FetchCommentTask;
@@ -38,18 +36,47 @@ public class CommentActivity extends BaseActivity {
 
     private CommentAdapter mAdapter;
 
-    private ArrayList<Comment> mLongComments;
+    private ArrayList<Comment> mComments = new ArrayList<>();
+
+    // 长评在评论集合中的起始位置
+    private int mLongCommentStartPos = -1;
+
+    // 短评在评论集合中的起始位置
+    private int mShortCommentStatPos = -1;
+
+    private int mLongCommentSize = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         StatusBarUtils.setStatusBarColor(this, getResources().getColor(R.color.colorPrimary));
         setToolbarTitle(R.string.activity_title_comment);
-        Latest latest = (Latest) getIntent().getSerializableExtra("latest");
+        final Latest latest = (Latest) getIntent().getSerializableExtra("latest");
         FetchCommentTask.fetch(latest.getId(), FetchCommentTask.TYPE_LONG_COMMENT, new FetchCommentTask.FetchLatestCallback() {
             @Override
             public void onSuccess(ArrayList<Comment> comments) {
-                onFetchSuccess(comments);
+                mComments.addAll(comments);
+                if (comments.size() != 0) {
+                    // 有长评
+                    mLongCommentStartPos = 0;
+                    mLongCommentSize = comments.size();
+                }
+                FetchCommentTask.fetch(latest.getId(), FetchCommentTask.TYPE_SHORT_COMMENT, new FetchCommentTask.FetchLatestCallback() {
+                    @Override
+                    public void onSuccess(ArrayList<Comment> comments) {
+                        mComments.addAll(comments);
+                        if (comments.size() != 0) {
+                            // 有短评
+                            mShortCommentStatPos = mLongCommentSize;
+                        }
+                        onFetchSuccess();
+                    }
+
+                    @Override
+                    public void onError(String errorMsg) {
+
+                    }
+                });
             }
 
             @Override
@@ -59,13 +86,14 @@ public class CommentActivity extends BaseActivity {
         });
     }
 
-    private void onFetchSuccess(ArrayList<Comment> comments) {
-        this.mLongComments = comments;
+    private void onFetchSuccess() {
+        Log.d("bingo", mComments.size() + "");
         initRecyclerView();
     }
 
     private void initRecyclerView() {
-        mAdapter = new CommentAdapter(this, mLongComments);
+        Log.d("bingo", "mLongCommentSize:" + mLongCommentSize + " mLongCommentStartPos:" + mLongCommentStartPos + " mShortCommentStatPos:" + mShortCommentStatPos);
+        mAdapter = new CommentAdapter(this, mComments, mLongCommentStartPos, mShortCommentStatPos);
         RecyclerView.LayoutManager lm = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(lm);
         mRecyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
